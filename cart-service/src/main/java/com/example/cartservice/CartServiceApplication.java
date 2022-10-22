9037aa9c9b0791936400857e67229c21ed30544c
Manish Rawat <manishrawat911@gmail.com>
2022-10-21 20:19:14 -0400

    Added Eureka Server and Clients

--- cart-service/src/main/java/com/example/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/example/cartservice/CartServiceApplication.java
@@ -1,11 +1,15 @@
 package com.example.cartservice;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class CartServiceApplication {
-
+    @Autowired
+    private EurekaClient eurekaClient;
     public static void main(String[] args) {
         SpringApplication.run(CartServiceApplication.class, args);
     }
